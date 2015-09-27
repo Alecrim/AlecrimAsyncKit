b703302693e60a4b6fb9662cab5ad28ec40b28ae
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2015-09-27 20:27:06 -0300

    Added ActivityTaskObserver

---
+++ Source/AlecrimAsyncKit/Convenience Observers/ActivityTaskObserver.swift
@@ -0,0 +1,28 @@
+//
+//  ActivityTaskObserver.swift
+//  AlecrimAsyncKit
+//
+//  Created by Vanderlei Martinelli on 2015-09-27.
+//  Copyright © 2015 Alecrim. All rights reserved.
+//
+
+import Foundation
+
+public final class ActivityTaskObserver: TaskObserver {
+    
+    public init(options: NSActivityOptions, reason: String) {
+        super.init()
+        
+        var activity: NSObjectProtocol!
+        
+        self.didStart { _ in
+            NSProcessInfo.processInfo().beginActivityWithOptions(options, reason: reason)
+        }
+        
+        self.didFinish { _ in
+            NSProcessInfo.processInfo().endActivity(activity)
+            activity = nil
+        }
+    }
+    
+}
