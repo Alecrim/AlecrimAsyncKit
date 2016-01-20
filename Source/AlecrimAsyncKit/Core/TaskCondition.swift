5c4d896a36ace61ed4d66f68d74c550f31b6d0a1
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2016-01-20 15:20:06 -0200

    Minor code refactoring

--- Source/AlecrimAsyncKit/Core/TaskCondition.swift
+++ Source/AlecrimAsyncKit/Core/TaskCondition.swift
@@ -131,24 +131,17 @@ public class TaskCondition {
 extension TaskCondition {
     
     internal static func asyncEvaluateConditions(conditions: [TaskCondition]) -> Task<Void> {
-        return asyncEx(_defaultTaskConditionQueue) { task in
-            do {
-                for condition in conditions {
-                    if let subconditions = condition.subconditions where !subconditions.isEmpty {
-                        try await(TaskCondition.asyncEvaluateConditions(subconditions))
-                    }
-                    
-                    if let dependencyTask = condition.dependencyTaskClosure() {
-                        try await(dependencyTask)
-                    }
-                    
-                    try await(condition.asyncEvaluate())
+        return async(_defaultTaskConditionQueue) {
+            for condition in conditions {
+                if let subconditions = condition.subconditions where !subconditions.isEmpty {
+                    try await(TaskCondition.asyncEvaluateConditions(subconditions))
                 }
                 
-                task.finish()
-            }
-            catch let error {
-                task.finishWithError(error)
+                if let dependencyTask = condition.dependencyTaskClosure() {
+                    try await(dependencyTask)
+                }
+                
+                try await(condition.asyncEvaluate())
             }
         }
     }
