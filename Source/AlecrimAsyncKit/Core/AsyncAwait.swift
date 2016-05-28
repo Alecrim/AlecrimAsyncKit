bdf0e1845860a013bc9bda0ef9ee244d7fc3b220
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2016-05-28 00:03:53 -0300

    Implemented some helper tasks

--- Source/AlecrimAsyncKit/Core/AsyncAwait.swift
+++ Source/AlecrimAsyncKit/Core/AsyncAwait.swift
@@ -95,8 +95,96 @@ public func await<V>(task: Task<V>) throws -> V {
     return task.value
 }
 
+// MARK: - Helper methods
 
-// MARK: - 
+private final class ThreadWithClosure: NSThread {
+    
+    private let closure: () -> Void
+    
+    private init(closure: () -> Void) {
+        self.closure = closure
+        super.init()
+    }
+    
+    private override func main() {
+        self.closure()
+    }
+    
+}
+
+@warn_unused_result
+public func delay(timeInterval: NSTimeInterval) -> NonFailableTask<Void> {
+    return sleep(forTimeInterval: timeInterval)
+}
+
+@warn_unused_result
+public func sleep(forTimeInterval ti: NSTimeInterval) -> NonFailableTask<Void> {
+    return asyncEx { t in
+        let thread = ThreadWithClosure {
+            NSThread.sleepForTimeInterval(ti)
+            t.finish()
+        }
+        
+        thread.start()
+    }
+}
+
+@warn_unused_result
+public func sleep(until date: NSDate) -> NonFailableTask<Void> {
+    return asyncEx { t in
+        let thread = ThreadWithClosure {
+            NSThread.sleepUntilDate(date)
+            t.finish()
+        }
+        
+        thread.start()
+    }
+}
+
+@warn_unused_result
+public func whenAll(tasks: [TaskProtocol]) -> Task<Void> {
+    return async {
+        for task in tasks {
+            task.waitUntilFinished()
+            
+            if let errorReportingTask = task as? ErrorReportingTask, let error = errorReportingTask.error {
+                throw error
+            }
+        }
+    }
+}
+
+@warn_unused_result
+public func whenAny(tasks: [TaskProtocol]) -> Task<TaskProtocol> {
+    return asyncEx { t in
+        func observeTask(task: TaskProtocol) throws -> Task<Void> {
+            return async {
+                task.waitUntilFinished()
+                
+                if let errorReportingTask = task as? ErrorReportingTask, let error = errorReportingTask.error {
+                    throw error
+                }
+                
+                t.finish(with: task)
+            }
+        }
+        
+        do {
+            for task in tasks {
+                if t.finished {
+                    break
+                }
+                
+                try observeTask(task)
+            }
+        }
+        catch let error {
+            t.finish(with: error)
+        }
+    }
+}
+
+// MARK: -
 
 private func createdTask<T: InitializableTask>(queue queue: NSOperationQueue, qualityOfService: NSQualityOfService?, taskPriority: TaskPriority?, conditions: [TaskCondition]?, observers: [TaskObserver]?, asynchronous: Bool, closure: (T) -> Void) -> T {
     assert(queue.maxConcurrentOperationCount == NSOperationQueueDefaultMaxConcurrentOperationCount || queue.maxConcurrentOperationCount > 1, "Task `queue` cannot be the main queue nor a serial queue.")
