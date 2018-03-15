8ab82b615d7ed8eac4ad9079f4a9998205d3a9f4
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2018-03-15 18:32:37 -0300

    Renamed OperationQueue convenience methods

--- Source/AlecrimAsyncKit/Convenience/OperationQueue+Extensions.swift
+++ Source/AlecrimAsyncKit/Convenience/OperationQueue+Extensions.swift
@@ -10,19 +10,19 @@ import Foundation
 
 extension OperationQueue {
     
-    public func async<Value>(execute closure: @escaping AsyncTaskClosure<Value>) -> Task<Value> {
+    public func addOperation<Value>(_ closure: @escaping AsyncTaskClosure<Value>) -> Task<Value> {
         return AlecrimAsyncKit.async(in: self, execute: closure)
     }
     
-    public func async<Value>(execute closure: @escaping AsyncNonFailableTaskClosure<Value>) -> NonFailableTask<Value> {
+    public func addOperation<Value>(_ closure: @escaping AsyncNonFailableTaskClosure<Value>) -> NonFailableTask<Value> {
         return AlecrimAsyncKit.async(in: self, execute: closure)
     }
     
-    public func async<Value>(execute taskClosure: @escaping AsyncTaskFullClosure<Value>) -> Task<Value> {
+    public func addOperation<Value>(_ taskClosure: @escaping AsyncTaskFullClosure<Value>) -> Task<Value> {
         return AlecrimAsyncKit.async(in: self, execute: taskClosure)
     }
     
-    public func async<Value>(execute taskClosure: @escaping AsyncTaskFullClosure<Value>) -> NonFailableTask<Value> {
+    public func addOperation<Value>(_ taskClosure: @escaping AsyncTaskFullClosure<Value>) -> NonFailableTask<Value> {
         return AlecrimAsyncKit.async(in: self, execute: taskClosure)
     }
     
