c962e293ab314ca45379448b6580d56adb9f2898
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2018-05-06 20:40:00 -0300

    Changed the max concurrent operation count for operation queues

--- Source/AlecrimAsyncKit/Core/Queue.swift
+++ Source/AlecrimAsyncKit/Core/Queue.swift
@@ -10,11 +10,14 @@ import Foundation
 
 internal /* namespace */ enum Queue {
 
+    //private static let defaultMaxConcurrentOperationCount = ProcessInfo().activeProcessorCount * 2
+    private static let defaultMaxConcurrentOperationCount = OperationQueue.defaultMaxConcurrentOperationCount
+
     internal static let defaultOperationQueue: OperationQueue = {
         let queue = OperationQueue()
         queue.name = "com.alecrim.AlecrimAsyncKit.Task"
         queue.qualityOfService = .utility
-        queue.maxConcurrentOperationCount = ProcessInfo().activeProcessorCount * 2
+        queue.maxConcurrentOperationCount = Queue.defaultMaxConcurrentOperationCount
 
         return queue
     }()
@@ -23,14 +26,14 @@ internal /* namespace */ enum Queue {
         let queue = OperationQueue()
         queue.name = "com.alecrim.AlecrimAsyncKit.TaskAwaiter"
         queue.qualityOfService = .utility
-        queue.maxConcurrentOperationCount = ProcessInfo().activeProcessorCount * 2
+        queue.maxConcurrentOperationCount = Queue.defaultMaxConcurrentOperationCount
 
         return queue
     }()
 
     internal static func operationQueue(for dispatchQueue: DispatchQueue) -> OperationQueue {
         let operationQueue = OperationQueue()
-        operationQueue.maxConcurrentOperationCount = ProcessInfo().activeProcessorCount * 2
+        operationQueue.maxConcurrentOperationCount = Queue.defaultMaxConcurrentOperationCount
         operationQueue.underlyingQueue = dispatchQueue
 
         return operationQueue
@@ -42,7 +45,7 @@ internal /* namespace */ enum Queue {
         let queue = OperationQueue()
         queue.name = "com.alecrim.AlecrimAsyncKit.TaskCondition"
         queue.qualityOfService = .utility
-        queue.maxConcurrentOperationCount = ProcessInfo().activeProcessorCount * 2
+        queue.maxConcurrentOperationCount = Queue.defaultMaxConcurrentOperationCount
 
         return queue
     }()
