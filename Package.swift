b1250328492af82fdefd1b2f090d3bd38ae96695
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2019-04-11 17:09:18 -0300

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,13 @@ import PackageDescription
 let package = Package(
     name: "AlecrimAsyncKit",
     platforms: [
-        .macOS(.v10_13)
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .watchOS(.v3),
+        .tvOS(.v10)
     ],
     products: [
-        .library(name: "AlecrimAsyncKit", targets: ["AlecrimAsyncKit"]),
+        .library(name: "AlecrimAsyncKit", targets: ["AlecrimAsyncKit"])
     ],
     targets: [
         .target(name: "AlecrimAsyncKit", path: "Sources")
